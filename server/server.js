95c1601db6181bc29c46671823ad15234ba5786a
MarcusKri <marcus-kristoffersen@outlook.com>
2022-02-24 14:07:34 +0100

    Add prettier and husky

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ import express from "express";
 const app = express();
 
 app.get("/login", (req, res, next) => {
-    res.json({username: "Marcus Jojo"})
-})
+  res.json({ username: "Marcus Jojo" });
+});
 app.post("/login", (req, res, next) => {
-    res.sendStatus(401);
-})
+  res.sendStatus(401);
+});
 
 const server = app.listen(process.env.PORT || 2000, () => {
-    console.log(`Started on http://localhost:${server.address().port}`)
-})
+  console.log(`Started on http://localhost:${server.address().port}`);
+});
